94920f5852a6cc90331ab43a6cea3f7791bab940
SEBILLE Benjamin <sebille.b@paris.sfeir.com>
2021-06-14 09:41:07 +0200

    add website title/logo to menubar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { createStyles, makeStyles } from '@material-ui/core';
 import React from 'react';
 import MenuBar from './MenuBar';
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    app: {
+      display: 'grid',
+      gridTemplateColumns: '20% 60% 20%',
+      gap: '10px',
+      '& *': {
+        border: '1px dotted',
+      }
+    },
+  }),
+);
+
 function App() {
+  const classes = useStyles();
 
   return (
-    <div className="App">
-        <MenuBar/>
+    <div>
+      <MenuBar/>
     </div>
   );
 }
--- src/MenuBar.tsx
+++ src/MenuBar.tsx
@@ -1,12 +1,23 @@
 import { Button, createStyles, makeStyles, withStyles } from '@material-ui/core';
 import { useState } from 'react';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { ToggleButton } from '@material-ui/lab';
 
 const useStyles = makeStyles(() =>
   createStyles({
-    menu: {
-      display: 'block',
-      textAlign: 'center',
+    header: {
+      display: 'flex',
+      justifyContent: 'space-around',
     },
+    menu: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    home: {
+      display: 'flex',
+      justifyContent: 'center',
+    }
   }),
 );
 
@@ -18,67 +29,87 @@ const MenuButton = withStyles({
     fontWeight: 'bold',
     textTransform: 'none',
     lineHeight: 'normal',
-    borderRadius: '40px',
+    height: '2em',
+    borderRadius: '15px',
+    border: '1px solid',
+    borderColor: '#E1E1E1',
     marginLeft: '10px',
     marginRight: '10px',
+    '&:hover': {
+      boxShadow: 'none',
+      backgroundColor: 'rgb(58, 170, 53)',
+      color: 'rgb(255, 255, 255)',
+    },
     '&:focus': {
       boxShadow: 'none',
       backgroundColor: 'rgb(58, 170, 53)',
-      borderColor: '#000000',
+      border: 'none',
+      color: 'rgb(255, 255, 255)',
+    },
+    '&.Mui-selected': {
+      boxShadow: 'none',
+      backgroundColor: 'rgb(58, 170, 53)',
+      border: 'none',
       color: 'rgb(255, 255, 255)',
     },
   },
+})(ToggleButton);
+
+const HomeButton = withStyles({
+  root: {
+    fontFamily: 'Poppins, Sans-serif',
+    fontSize: '25px',
+    fontWeight: 'bold',
+    textTransform: 'none',
+    '&:hover': {
+      backgroundColor: 'white',
+    }
+  },
 })(Button);
+
+const HomeIcon = withStyles({
+  root: {
+    color : 'rgb(58, 170, 53)',
+  },
+})(LocationOnIcon);
 
 function MenuBar() {
   const classes = useStyles();
   const [activeItem, setActiveMenu] = useState('home');
 
-  const handleHomeButtonClick = (): void => {
-    setActiveMenu('home');
-  }
-
-  const handleAgendaButtonClick = (): void => {
-    setActiveMenu('agenda');
-  }
-
-  const handleRepertoireButtonClick = (): void => {
-    setActiveMenu('repertoire');
-  }
-
-  const handleReportagesButtonClick = (): void => {
-    setActiveMenu('reportages');
-  }
-
-  const handleForumButtonClick = (): void => {
-    setActiveMenu('forum');
-  }
-
-  const handleContactsButtonClick = (): void => {
-    setActiveMenu('contacts');
+  const handleMenuButtonClick = (value: string): void => {
+    setActiveMenu(value);
   }
 
   return (
+    <div className={classes.header}>
+      <div className={classes.home}>
+        <HomeButton>
+          <HomeIcon fontSize='large' />
+          choix d'ici
+        </HomeButton>
+      </div>
       <div className={classes.menu}>
-        <MenuButton onClick={handleHomeButtonClick}>
+        <MenuButton onClick={() => handleMenuButtonClick('home')} selected={activeItem === 'home'} >
           Accueil
         </MenuButton>
-        <MenuButton onClick={handleAgendaButtonClick}>
+        <MenuButton onClick={() => handleMenuButtonClick('agenda')} selected={activeItem === 'agenda'} >
           Agenda
         </MenuButton>
-        <MenuButton onClick={handleRepertoireButtonClick}>
+        <MenuButton onClick={() => handleMenuButtonClick('repertoire')} selected={activeItem === 'repertoire'} >
           Répertoire
         </MenuButton>
-        <MenuButton onClick={handleReportagesButtonClick}>
+        <MenuButton onClick={() => handleMenuButtonClick('reportages')} selected={activeItem === 'reportages'} >
           Reportages
         </MenuButton>
-        <MenuButton onClick={handleForumButtonClick}>
+        <MenuButton onClick={() => handleMenuButtonClick('forum')} selected={activeItem === 'forum'} >
           Forum
         </MenuButton>
-        <MenuButton onClick={handleContactsButtonClick}>
+        <MenuButton onClick={() => handleMenuButtonClick('contacts')} selected={activeItem === 'contacts'} >
           Contacts
         </MenuButton>
       </div>
+    </div>
   );
 }
 
