3fd63426313a6321ce82a8223eabfb8de4be4f93
benjaminSeb <sebille.benjamin@live.fr>
2021-08-12 19:06:23 +0200

    Delete choixdici-firestore.json

---
+++ README.md
@@ -0,0 +1,28 @@
+# Choix d'Ici
+
+Application Web Choix d'Ici
+
+# Git Flow
+
+[Git Flow - Atlassian](https://www.atlassian.com/fr/git/tutorials/comparing-workflows/gitflow-workflow)
+
+## Available Scripts
+
+See package.json in front or back directory.
+
+### Start Back
+
+Attention : Nécessite la clé d'accès à la base de données  
+`nodemon server`  
+Call [localhost:3001](localhost:3001)
+
+### Start Front
+
+`npm start`  
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+#### Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
--- back/app.js
+++ back/app.js
@@ -1,12 +1,5 @@
 const express = require('express');
-// const admin = require('firebase-admin');
-// const serviceAccount = require('./choixdici-firestore.json');
 const firestoreRoutes = require('./routes/firestore');
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// });
-// const db = admin.firestore();
 
 const app = express();
 
@@ -18,55 +11,6 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-
-// app.use('/create', async (req, res, next) => {
-//     const docRef = db.collection('users').doc('alovelace');
-
-//     await docRef.set({
-//         first: 'Ada',
-//         last: 'Lovelace',
-//         born: 1815
-//     });
-
-//     res.status(201).json({
-//         message: 'Objet créé dans Firestore !'
-//     });
-// });
-
-// app.use('/update', async (req, res, next) => {
-//     const docRef = db.collection('users').doc('alovelace');
-
-//     await docRef.set({
-//         first: 'Zbra',
-//         last: 'Sting',
-//         born: 2021
-//     });
-
-//     res.status(201).json({
-//         message: 'Objet MAJ dans Firestore !'
-//     });
-// });
-
-// app.use('/get', async (req, res, next) => {
-//     const snapshot = await db.collection('users').get();
-//     snapshot.forEach((doc) => {
-//         console.log(doc.id, '=>', doc.data());
-//     });
-//     const datas = snapshot.docs.map((doc) => {
-//         rObj = {};
-//         rObj[doc.id] = doc.data();
-//         return rObj;
-//     })
-//     res.status(200).json(datas);
-// });
-
-// app.use('/delete', async (req, res, next) => {
-//     const deleted = await db.collection('users').doc('alovelace').delete();
-
-//     res.status(200).json({
-//         message: 'Objet supprimé dans Firestore !'
-//     });
-// })
 
 app.use('/firestore', firestoreRoutes);
 
